f17ffffcb7921bc3d4ff57c9d4eaccd81aa8267f
lccgjr <123847140+lccgjr@users.noreply.github.com>
2023-02-27 16:27:11 -0300

    Add files via upload

--- principal.py
+++ principal.py
@@ -4,11 +4,31 @@
 # primeiro solicita a digitação de um email válido
 #
 # ================================================
-solicitaemail = str(input('Digite um email válido (formato nome@dominio)\n'))
-print (solicitaemail)
+#
+#
+import re
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+#
+# Modulo para testar se email é válido
+#
+#
+def testa(email):
+    if(re.search(regex,email)):
+        return True
+    else:
+        return False
+#
+# 
+#
+email = str(input('Digite um email válido (formato nome@dominio)\n'))
+print (email)
 #
 # Chama o módulo que testa se o email é válido
 #
-import testaemail
-testaemail.testa(solicitaemail)
+testa(email)
+if testa(email):
+    print('valido')
+else:
+    print('invalido')
 #
+# Acessa banco de dados e verifica se email está cadastrado
